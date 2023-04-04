99d07d02f6feeec6c60ba59f12ee2428bf398a75
AN DO <anmana000@gmail.com>
2023-04-04 22:28:30 +0700

    lemmatization stopwords concurrency

--- main.py
+++ main.py
@@ -7,12 +7,14 @@
 import sys
 import requests
 import json
-from tqdm import tqdm
 import re
 import time
 import os
 from collections import Counter
 import math
+import concurrent.futures
+import nltk
+lemma = nltk.wordnet.WordNetLemmatizer()
 
 HEADERS = {
     "User-Agent": "python:com.example.trendseeker:v0.0.1 (by /u/No_Musician_4234)"
@@ -33,8 +35,8 @@ WEAPONS = [
     ("hbg", "heavybowgun"),
     ("bow", "bow"),
     ("gs", "greatsword"),
-
 ]
+WPN_SHORT = set([i for j in WEAPONS for i in j])
 
 
 def getWeaponType(string):
@@ -78,50 +80,62 @@ class Scraper:
             "parent_id": parent_id
         })
 
+    def scrape(self, total, name, limit, now):
+        after = "="
+        filtered = {
+            "parents": {},
+            "comments": [],
+            "users": {}
+        }
+        url = f"https://dm.reddit.com/r/{name}/comments.json?limit={limit}&count=100&after=="
+        for _ in range(0, total, limit):
+            try:
+                print(url)
+                res = requests.get(url, headers=HEADERS)
+                data = res.json()
+                url = url[:-len(after)] + data.get("data").get("after")
+                after = data.get("data").get("after")
+                for child in data["data"].get("children"):
+                    child = child.get("data")
+                    self._getCommentMetadata(child, filtered)
+                    self._getParentMetadata(child, filtered)
+                    self._getUserMetadata(child, filtered)
+
+                with open(f"./raw/{now}/{name}_{after}.json", "w+") as f:
+                    f.write(json.dumps(data))
+
+            except Exception as e:
+                print(f"ERROR: something happened {e}")
+                break
+
+        # with open(f"./filtered/{int(time.time())}_{name}.json", "w+") as f:
+        #     f.write(json.dumps(filtered))
+
     def scrapeAllComments(self, total=100):
         limit = 100
-        for name in self.subreddits:
-            after = "="
-            filtered = {
-                "parents": {},
-                "comments": [],
-                "users": {}
-            }
-            url = f"https://dm.reddit.com/r/{name}/comments.json?limit={limit}&count=100&after=="
-            now = int(time.time())
-            os.makedirs(f"./raw/{now}")
-            for _ in tqdm(range(0, total, limit)):
-                try:
-                    res = requests.get(url, headers=HEADERS)
-                    data = res.json()
-                    url = url[:-len(after)] + data.get("data").get("after")
-                    after = data.get("data").get("after")
-                    for child in data["data"].get("children"):
-                        child = child.get("data")
-                        self._getCommentMetadata(child, filtered)
-                        self._getParentMetadata(child, filtered)
-                        self._getUserMetadata(child, filtered)
+        now = int(time.time())
+        os.makedirs(f"./raw/{now}")
+        with concurrent.futures.ThreadPoolExecutor(max_workers=4) as executor:
+            for name in self.subreddits:
+                executor.submit(self.scrape, total, name, limit, now)
 
-                    with open(f"./raw/{now}/{after}.json", "w+") as f:
-                        f.write(json.dumps(data))
-
-                except Exception as e:
-                    print(f"ERROR: something happened {e}")
-                    break
-
-            with open(f"./filtered/{int(time.time())}_{name}.json", "w+") as f:
-                f.write(json.dumps(filtered))
+            # a = Scraper()
+            # a.scrapeAllComments(10000)
 
 
-# a = Scraper()
-# a.scrapeAllComments(10000)
 def clean(sentence):
-    return re.sub("[^'a-zA-Z\\d:]", " ", sentence)
+    return re.sub("[^'a-zA-Zd: ]", "", sentence)
+
+
+stopwords = set()
+with open("stopwords.txt", "r") as f:
+    stopwords = set(f.read().splitlines())
 
 
 def tokenize(sentence):
-    n1 = [word.lower().strip() for word in clean(sentence).split(" ")]
-    n2 = [f"{n1[i]} {n1[i+1]}" for i in range(len(n1) - 2 + 1)]
+    n1 = [word.lower().strip() for word in clean(sentence).split(" ") if word.lower().strip() not in stopwords]
+    n1 = [lemma.lemmatize(word) if word not in WPN_SHORT else word for word in n1]
+    n2 = [f"{n1[i]} {n1[i+1]}" for i in range(len(n1) - 2 + 1) if n1[i] and n1[i+1]]
     return n2
 
 
@@ -150,23 +164,34 @@ class Stuff:
         return sorted_tf_idf, tf
 
     def wac(self):
-        path = "/mnt/SSDee/work/baking/redditmhbot/filtered/"
+        path = "/Users/ando/work/baking/redditbot/filtered/"
         filenames = os.listdir(path)
         curr = int(time.time())
         wac = sorted([(int(i[:10]), i)
                       for i in filenames], key=lambda x: curr - x[0])
 
-        a = {}
+        corpus = {
+            "MonsterHunter": {},
+            "MHRise": {},
+            "MonsterHunterMeta": {}
+        }
         for _, name in wac:
+            a = {}
             with open(path + name, "r") as f:
+                category = name.split("_")[1].split(".")[0]
+
                 data = json.load(f)
                 for i in data["parents"]:
                     a[i] = [data["parents"][i]]
 
                 for comment in data["comments"]:
                     a[comment["parent_id"]].append(comment["body"])
+                corpus[category] = {**corpus[category], **a}
 
-                sorted_tf_idf, tf = self.tf_idf([a[i] for i in a])
+        for cat in corpus:
+            print(f"______{cat}_____")
+            sorted_tf_idf, tf = self.tf_idf([corpus[cat][i] for i in corpus[cat]])
+            print([i for i in sorted_tf_idf][:30])
 
 
 if __name__ == '__main__':
@@ -174,13 +199,19 @@ if __name__ == '__main__':
         action = sys.argv[1]
         if action == "scrape":
             a = Scraper()
-            a.scrapeAllComments(10000)
+            a.scrapeAllComments(900)
         elif action == "idf":
             a = Stuff()
             a.wac()
+        elif action == "all":
+            a = Scraper()
+            a.scrapeAllComments(900)
+            b = Stuff()
+            b.wac()
     except IndexError:
-        a = Stuff()
-        a.wac()
+        # a = Stuff()
+        # a.wac()
+        print(stopwords)
         # print("what's up dude")
 # a = Stuff()
 # a.wac()
