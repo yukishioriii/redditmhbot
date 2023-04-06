039ba69490dbfeeedd8fc31e2b0afb008ec1f24e
AN DO <anmana000@gmail.com>
2023-04-06 13:17:37 +0700

    weighted on time created

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 """
 # bot to scrape entire monster hunter mhrise subreddit, figure out if there's a trend going on
-
 """
 
 import sys
@@ -18,6 +17,9 @@ lemma = nltk.wordnet.WordNetLemmatizer()
 HEADERS = {
     "User-Agent": "python:com.example.trendseeker:v0.0.1 (by /u/No_Musician_4234)"
 }
+
+ONE_WEEK_B4 = int(time.time()) - (60*60*24*7)
+
 
 WEAPONS = [
     ("ls", "longsword"),
@@ -139,28 +141,41 @@ with open("stopwords.txt", "r") as f:
     stopwords = set(f.read().splitlines())
 
 
-def tokenize(sentence):
-    n1 = [word.lower().strip() for word in clean(sentence).split(" ")
-          if word.lower().strip() not in stopwords]
-    n1 = [lemma.lemmatize(
-        word) if word not in WPN_SHORT else word for word in n1]
-    n2 = [
-        f"{n1[i]} {n1[i+1]}" for i in range(len(n1) - 2 + 1) if n1[i] and n1[i+1]]
-    return n2
-
-
-def tokenizeSentences(a):
-    latest_date_used = {}
-    return [w for i in a for w in tokenize(i, latest_date_used)]
-
-
 class Stuff:
+    latest_date_used = {}
+    now = time.time()
+
+    def tokenize(self, score, date, sentence):
+        if date is None:
+            date = ONE_WEEK_B4
+        n1 = [word.lower().strip() for word in clean(sentence).split(" ")
+              if word.lower().strip() not in stopwords]
+        n1 = [lemma.lemmatize(
+            word) if word not in WPN_SHORT else word for word in n1]
+        n2 = []
+        for i in range(len(n1) - 2 + 1):
+            if not (n1[i] and n1[i+1]):
+                continue
+            word = f"{n1[i]} {n1[i+1]}"
+            n2.append(word)
+            if not self.latest_date_used.get(word) or date > self.latest_date_used[word]:
+                self.latest_date_used[word] = date
+
+        return n2
+
+    def tokenizeSentences(self, a):
+        return [w for (score, date, sentence) in a for w in self.tokenize(score, date, sentence)]
+
+    def weight(self, word):
+        days_elapsed = (self.now - self.latest_date_used[word]) // 50400
+        return 6 / (days_elapsed + 3)
+
     def tf_idf(self, corpus):
         tf = Counter(
-            [w for documents in corpus for w in tokenizeSentences(documents)])
+            [w for s in corpus for w in self.tokenizeSentences(s)])
         df = {}
         for documents in corpus:
-            ws = set(tokenizeSentences(documents))
+            ws = set(self.tokenizeSentences(documents))
             for w in ws:
                 if w in df:
                     df[w].append(documents)
@@ -168,13 +183,13 @@ class Stuff:
                     df[w] = [documents]
         len_doc = len(corpus)
         idf = {i: math.log(len_doc / len(df[i])) for i in df}
-        tf_idf = {i: math.log(tf[i]) * idf[i] for i in idf}
+        tf_idf = {i: math.log(tf[i]) * self.weight(i) * idf[i] for i in idf}
         sorted_tf_idf = {k: v for k, v in sorted(
             tf_idf.items(), key=lambda x: -x[1])}
         return sorted_tf_idf, tf
 
     def wac(self):
-        path = "/mnt/SSDee/work/baking/redditmhbot/filtered/"
+        path = "./filtered/"
         filenames = os.listdir(path)
         curr = int(time.time())
         wac = sorted([(int(i[:10]), i)
@@ -190,7 +205,6 @@ class Stuff:
             corpus[i]["comments"] = {}
             corpus[i]["users"] = {}
 
-        seven_day_b7 = int(time.time()) - (60*60*24*7)
         for _, name in wac:
             a = {}
             with open(path + name, "r") as f:
@@ -208,19 +222,17 @@ class Stuff:
                 corpus[category]["comments"] = {
                     **corpus[category]["comments"], **comments}
 
-                parents = {i:[(None,None, data["parents"][i])] for i in data["parents"]}
+                parents = {i: [(None, None, data["parents"][i])] for i in data["parents"]}
 
                 for i in data["comments"]:
-                    parents[i["parent_id"]].append((i.get("score", 0), i.get("created", seven_day_b7), i["body"]))
-                
+                    parents[i["parent_id"]].append((i.get("score", 0), i.get("created", ONE_WEEK_B4), i["body"]))
+
                 corpus[category]["parents"] = {
                     **corpus[category]["parents"], **parents}
-        print()
 
         for cat in corpus:
             print(f"______{cat}_____")
-            sorted_tf_idf, tf = self.tf_idf(
-                [corpus[cat][i] for i in corpus[cat]])
+            sorted_tf_idf, tf = self.tf_idf([i for i in corpus[cat]["parents"].values()])
             print([i for i in sorted_tf_idf][:30])
 
 
